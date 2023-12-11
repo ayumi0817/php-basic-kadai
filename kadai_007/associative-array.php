04a0eeee1abd315641c9785e2c361ce6f0ab5b99
ayumi0817 <jinpo.ayumi@gmail.com>
2023-12-11 23:13:08 +0900

    コミット

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+ <html lang="ja">
+ 
+ <head>
+     <meta charset="UTF-8">
+     <title>kadai_007</title>
+ </head>
+ 
+ <body>
+    <p>
+         <?php
+         // 変数を定義する
+         $Renso=["name"=>"onion", "price"=>200, "weight"=>160];
+         print_r($Renso);
+         ?>
+    </p>
+ </body>
+ 
+ </html>

68bdc754691b82c65463f7ad67868ccb472dd8c7
ayumi0817 <jinpo.ayumi@gmail.com>
2023-12-16 11:34:31 +0900

    コミット

---
+++ kadai_016/class.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html>
+    <head>
+      <title></title>
+      <meta charset="UTF-8">
+    </head>
+    <body>
+        <p>
+            <?php
+             class Foods{
+                public $name;
+                public $price;
+            }
+
+            $Food = new Foods();
+            $Food->name="potate";
+            $Food->price="250";
+
+            print_r($Food);
+            echo "<br>";
+
+             class Animals{
+                public $name;
+                public $height;
+                public $weight; 
+             }
+
+
+
+            $Animal=new Animals();
+                $Animal->name="dog";
+                $Animal->height="60";
+                $Animal->weight="5000";
+
+            print_r($Animal);
+            echo "<br>";
+
+            function showFood($Food){
+            echo $Food->price;
+            }
+            showFood($Food);
+            echo "<br>";
+
+
+            function showAnimal($Animal){
+            echo $Animal->height;
+            }
+            showAnimal($Animal);
+            echo "<br>";
+
+            ?>
+        </p>
+    </body>
+</html>
+
